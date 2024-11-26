20478caa8a244a1d328dcf496c4036584ec64eb2
Sanzai-X <130345226+Sanzai-X@users.noreply.github.com>
2024-11-26 18:40:55 +0800

    feat: multiply values of Vector A and B
    This commit will change the previous implementation of the file

--- asmdotproduct.asm
+++ asmdotproduct.asm
@@ -1,15 +1,18 @@
 section .text
 bits 64
 default rel
-global asmdotproduct
+global dotproduct ; rename this to asmdotproduct
 
-asmdotproduct:
-    ; A@rdi, B@rsi, sdot@rdx, n@rcx
-    ; registers 
+dotproduct:
 
-.loop:
-    ; arithmetic 
-
-.exit
-    ; Store result
+    L1:
+        movss xmm0, [rdx]
+        movss xmm1, [r8]
+        mulss xmm0, xmm1
+        movss [r9], xmm0
+        add rdx, 4
+        add r8, 4
+        add r9, 4
+        loop L1
+    
     ret
