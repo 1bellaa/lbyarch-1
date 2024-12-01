1303ca4a1fbe20d272f87d955daf132226b5d277
Sanzai-X <130345226+Sanzai-X@users.noreply.github.com>
2024-12-01 23:27:22 +0800

    update: add and mdoify
    - added a addition operation for the 2 vectors which is then stored in the memory [r9] outside the loop

--- asmdotproduct.asm
+++ asmdotproduct.asm
@@ -8,11 +8,11 @@ dotproduct:
     L1:
         movss xmm0, [rdx]
         movss xmm1, [r8]
-        mulss xmm0, xmm1
-        movss [r9], xmm0
+        mulss xmm0, xmm1        
+		addss xmm2, xmm0
         add rdx, 4
         add r8, 4
-        add r9, 4
         loop L1
-    
+    movss [r9], xmm2
+	
     ret
